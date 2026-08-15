ea8cc79412011a8b08857897bb51e3fa6531a24c
HtetShine1868 <hetaungshine25@gmail.com>
2026-08-16 01:06:22 +0630

    change deploy be link

--- src/components/auth/OAuthButtons.tsx
+++ src/components/auth/OAuthButtons.tsx
@@ -1,4 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://ecommercebe-xhhj.onrender.com";
 
 interface OAuthButtonsProps {
   /** Text variant: "sign in" vs "sign up" */
